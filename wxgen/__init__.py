2240a2c091a9f448bf3ad7365a734acbf58e3f7c
Thomas Nipen <tnipen@gmail.com>
2016-09-30 16:18:41 +0200

    Use setup.py for installation

---
+++ wxgen/__init__.py
@@ -0,0 +1,9 @@
+import sys
+import wxgen.driver as driver
+
+
+def main():
+   driver.run(sys.argv)
+
+if __name__ == '__main__':
+   main()
---
+++ wxgen/driver.py
@@ -0,0 +1,38 @@
+import sys
+import matplotlib.pylab as mpl
+import database
+from trajectory import *
+from generator import *
+import verif
+
+def run(argv):
+   if len(sys.argv) < 3:
+      print "Weather generator"
+      print "usage: wxgen N T"
+      print ""
+      print "Arguments:"
+      print "  N: Number of trajectories"
+      print "  T: Number of days in trajectory"
+
+      sys.exit()
+
+   N = int(sys.argv[1])
+   T = int(sys.argv[2])
+   #np.random.seed(1)
+
+   db = database.Random()
+
+   generator = Generator(db)
+   initial_state = {"T": 0+np.zeros(1, float)}
+   trajectories = [generator.get(T, initial_state) for i in range(0, N)]
+
+   #for tr in trajectories:
+      #mpl.plot(tr["T"], 'k.-', lw=0.5)
+      #mpl.plot(np.diff(tr))
+
+   v = verif.Change()
+   mpl.plot(np.linspace(0.5, T-0.5, T-1), v.compute(trajectories), 'k.-')
+   mpl.show()
+
+if __name__ == '__main__':
+       main()
