f6c6aabe7df2828de2136e32159c9760c6f765a8
Thomas Nipen <tnipen@gmail.com>
2017-02-27 10:14:25 +0100

    Added combo climate_model

---
+++ wxgen/tests/climate_model_test.py
@@ -0,0 +1,40 @@
+import unittest
+import wxgen.climate_model
+import numpy as np
+
+
+class BinTest(unittest.TestCase):
+   def test_simple(self):
+      model = wxgen.climate_model.Bin(10)
+      unixtimes = np.array([wxgen.util.date_to_unixtime(20150101),
+         wxgen.util.date_to_unixtime(20150110),
+         wxgen.util.date_to_unixtime(20150111)])
+      states = model.get(unixtimes)
+
+      np.array_equal([0,0,1], states)
+
+   def test_single(self):
+      model = wxgen.climate_model.Bin(10)
+      unixtimes = np.array([wxgen.util.date_to_unixtime(20150111)])
+      states = model.get(unixtimes)
+
+      np.array_equal([1], states)
+
+
+class ComboTest(unittest.TestCase):
+   def test_simple(self):
+      models = [wxgen.climate_model.Bin(10), wxgen.climate_model.Bin(12)]
+      model = wxgen.climate_model.Combo(models)
+      unixtimes = np.array([wxgen.util.date_to_unixtime(20150101),
+         wxgen.util.date_to_unixtime(20150110),
+         wxgen.util.date_to_unixtime(20150111),
+         wxgen.util.date_to_unixtime(20150112),
+         wxgen.util.date_to_unixtime(20150113)])
+      states = model.get(unixtimes)
+      self.assertEqual(5, states.shape[0])
+      self.assertEqual(2, states.shape[1])
+      np.array_equal([[0,0],[0,0],[1,0],[1,0],[1,1]], states)
+
+
+if __name__ == '__main__':
+   unittest.main()
