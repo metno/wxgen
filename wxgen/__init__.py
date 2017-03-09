0499c89577dbc581b72ca0147129431d05bff438
Thomas Nipen <tnipen@gmail.com>
2017-03-09 10:51:31 +0100

    Remove driver_db.py

--- wxgen/__init__.py
+++ wxgen/__init__.py
@@ -1,13 +1,9 @@
 import sys
 import wxgen.driver
-import wxgen.driver_db
 
 
 def main():
    wxgen.driver.main(sys.argv)
-
-def main_db():
-   wxgen.driver_db.run(sys.argv)
 
 if __name__ == '__main__':
    main()
