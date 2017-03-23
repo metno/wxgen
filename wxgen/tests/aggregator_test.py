fd934bf337daf89ccdacdce1893879f757fbe6b0
Thomas Nipen <tnipen@gmail.com>
2017-03-23 09:26:21 +0100

    Added axis argument to aggregators

--- wxgen/tests/aggregator_test.py
+++ wxgen/tests/aggregator_test.py
@@ -7,14 +7,22 @@ import numpy as np
 class AggregatorTest(unittest.TestCase):
    def test_consecutive(self):
       agg = wxgen.aggregator.Consecutive()
-      self.assertEqual(agg(np.array([0, 0, 1, 1, 1, 0, 0, 0])), 3)
-      self.assertEqual(agg(np.array([0, 0, 0, 0, 1, 0, 0, 0])), 4)
-      self.assertEqual(agg(np.array([1, 1, 1])), 0)
-      self.assertEqual(agg(np.array([0, 0])), 2)
-      self.assertEqual(agg(np.array([1, 0, 1, 0, 1, 0, 0])), 2)
-      self.assertEqual(agg(np.array([1, 0, 1, 0, 1, 0])), 1)
-      self.assertEqual(agg(np.array([0])), 1)
-      self.assertEqual(agg(np.array([1])), 0)
+      self.assertEqual(agg(np.array([1, 1, 0, 0, 0, 1, 1, 1])), 3)
+      self.assertEqual(agg(np.array([1, 1, 1, 1, 0, 1, 1, 1])), 4)
+      self.assertEqual(agg(np.array([0, 0, 0])), 0)
+      self.assertEqual(agg(np.array([1, 1])), 2)
+      self.assertEqual(agg(np.array([0, 1, 0, 1, 0, 1, 1])), 2)
+      self.assertEqual(agg(np.array([0, 1, 0, 1, 0, 1])), 1)
+      self.assertEqual(agg(np.array([1])), 1)
+      self.assertEqual(agg(np.array([0])), 0)
+
+   def test_consecutive_axis(self):
+      agg = wxgen.aggregator.Consecutive()
+      # self.assertEqual(agg(np.array([1, 1, 0, 0, 0, 1, 1, 1]), axis=0), 3)
+      ar = np.array([[1, 0, 0], [1, 1, 0]])
+      self.assertTrue(np.array_equal(agg(ar, axis=0), [2, 1, 0]))
+      self.assertTrue(np.array_equal(agg(ar, axis=1), [1, 2]))
+      np.array_equal(agg(np.array([1, 1, 0]), axis=0), [1, 1, 0])
 
 
 if __name__ == '__main__':
