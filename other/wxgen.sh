caf522731989c68888372d29c41b6f7b0f3fbf3d
Thomas Nipen <tnipen@gmail.com>
2017-05-27 00:13:32 +0200

    Updated the bash completion script

--- other/wxgen.sh
+++ other/wxgen.sh
@@ -14,23 +14,23 @@ elif [ "$mode" = "sim" ]; then
    elif [ "$prev" = "-m" ]; then
       COMPREPLY=( $( compgen -W "exp rmsd " -- $cur ) )
    elif [ "$cur" = "" ] || [[ "$cur" =~ -* ]]; then
-      COMPREPLY=( $( compgen -W "-h -n -t -m -rs -w -i -j -b -p -db -dbtype -o -wl -v --debug -s -lat -lon " -- $cur ) )
+      COMPREPLY=( $( compgen -f -W "-h -n -t -m -rs -w -i -j -b -p -db -dbtype --dbtype -o -wl -v --debug -s -lat -lon -mem " -- $cur ) )
    fi
 elif [ "$mode" = "truth" ]; then
    if [ "$prev" = "-db" ]; then
       COMPREPLY=( $( compgen -f -W -- $cur ) )
    elif [ "$cur" = "" ] || [[ "$cur" =~ -* ]]; then
-      COMPREPLY=( $( compgen -W "-h -sd -ed -n -t -db -dbtype --db -o -wl -v --debug -s -lat -lon " -- $cur ) )
+      COMPREPLY=( $( compgen -f -W "-h -sd -ed -n -t -db -dbtype --dbtype -o -wl -v --debug -s -lat -lon -mem " -- $cur ) )
    fi
 elif [ "$mode" = "verif" ]; then
-   if [ "$prev" = "-m" ]; then
+   if [ "$prev" = "-a" ]; then
       COMPREPLY=( $( compgen -W "consecutive iqr max mean median min range std sum variance " -- $cur ) )
-   elif [ "$prev" = "-m" ]; then
+   elif [ "$prev" = "-tr" ]; then
       COMPREPLY=( $( compgen -W "dryday frostday nothing summerday wetday " -- $cur ) )
    elif [ "$prev" = "-m" ]; then
-      COMPREPLY=( $( compgen -W "covarmap histogram jump map sortstat timestat timeseries variance " -- $cur ) )
+      COMPREPLY=( $( compgen -W "autocorr covarmap histogram jump map sortstat timestat timeseries variance " -- $cur ) )
    elif [ "$cur" = "" ] || [[ "$cur" =~ -* ]]; then
-      COMPREPLY=( $( compgen -W "-h -fs -m -o -truth -xlim -xlog -ylim -ylog -r -tr -a -clim -cmap -tm -ts -v --debug -s -lat -lon " -- $cur ) )
+      COMPREPLY=( $( compgen -f -W "-h -fs -m -o -xlim -xlog -ylim -ylog -r -tr -a -clim -cmap -tm -ts -v --debug -s -lat -lon -mem " -- $cur ) )
    fi
 fi
 return 0
