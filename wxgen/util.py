a6c56cb6e799cc49733225687719ed75c55c3ade
Rune Yoshida Tollefsrud <RuneYoshida.Tollefsrud@statkraft.com>
2017-06-15 14:18:30 +0200

    Revisited  division, reverted to normal / for functions where float is expected rather than int

--- wxgen/util.py
+++ wxgen/util.py
@@ -235,13 +235,13 @@ def distance(lat1, lon1, lat2, lon2):
    """
    # Convert from degrees to radians
    pi = 3.14159265
-   lon1 = lon1 * 2 * pi // 360
-   lat1 = lat1 * 2 * pi // 360
-   lon2 = lon2 * 2 * pi // 360
-   lat2 = lat2 * 2 * pi // 360
+   lon1 = lon1 * 2 * pi / 360
+   lat1 = lat1 * 2 * pi / 360
+   lon2 = lon2 * 2 * pi / 360
+   lat2 = lat2 * 2 * pi / 360
    dlon = lon2 - lon1
    dlat = lat2 - lat1
-   a = np.sin(dlat // 2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon // 2)**2
+   a = np.sin(dlat / 2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2)**2
    c = 2 * np.arcsin(np.sqrt(a))
    distance = 6.367e6 * c
    return distance
